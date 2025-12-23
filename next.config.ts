e393f6d567129a2f5929c89517e2325b300ed0f1
Gaurav Thakur <gthakur581@gmail.com>
2025-12-23 23:31:23 +0530

    Update dependencies, optimize configuration, and improve compatibility
    Updated project dependencies including `framer-motion`, `react-icons`, and `tailwind-merge`. Optimized key configurations such as switching `jsx` to `react-jsx` in `tsconfig.json` and adjusting rewrites in `next.config.ts`. Enhanced compatibility with updated packages and improved maintenance structure.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import rehypeSlug from "rehype-slug";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  experimental: { ppr: true, reactCompiler: true },
   rewrites: async () => {
     return {
       beforeFiles: [{ source: "/", destination: "https://gaurav.ensite.dev" }],
