b54f0ff7657ccb081e797729db7a58c1e1cc6fcd
Gaurav Thakur <gthakur581@gmail.com>
2025-12-23 23:44:49 +0530

    Enable experimental `viewTransition` API in `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  experimental: { viewTransition: true },
   rewrites: async () => {
     return {
       beforeFiles: [{ source: "/", destination: "https://gaurav.ensite.dev" }],
