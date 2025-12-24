ec07bc56db88816ab51572c27a37dbc0c01e5a91
Gaurav Thakur <gthakur581@gmail.com>
2025-12-24 22:01:18 +0530

    Update `mobile-menu.tsx`: use `label` instead of `name` for link keys and labels

--- src/app/_components/mobile-menu.tsx
+++ src/app/_components/mobile-menu.tsx
@@ -13,16 +13,16 @@ export function MobileMenu() {
           <IoMdMore fontSize={20} />
         </button>
       </Popover.Trigger>
-      <Popover.Content className="mr-2 mt-2 min-w-48 rounded-lg border bg-white p-2 shadow-sm outline-red-600">
+      <Popover.Content className="mt-2 mr-2 min-w-48 rounded-lg border bg-white p-2 shadow-sm outline-red-600">
         <ul className="flex flex-col text-sm">
           {links.map((link) => (
-            <li key={link.name} className="w-full">
+            <li key={link.label} className="w-full">
               <Popover.Close asChild>
                 <Link
                   href={link.href}
                   className="inline-block w-full rounded-lg p-2 outline-red-600 hover:bg-red-50"
                 >
-                  {link.name}
+                  {link.label}
                 </Link>
               </Popover.Close>
             </li>
