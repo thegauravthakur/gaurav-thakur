42f10de201acb87f956bd3bd2f356b3bb719b87e
Gaurav Thakur <gthakur581@gmail.com>
2025-12-24 19:52:06 +0530

    Remove `output: "export"` from `next.config.ts` to simplify configuration.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   experimental: { viewTransition: true },
   cacheComponents: true,
-  output: "export",
   // rewrites: async () => {
   //   return {
   //     beforeFiles: [{ source: "/", destination: "https://gaurav.ensite.dev" }],
