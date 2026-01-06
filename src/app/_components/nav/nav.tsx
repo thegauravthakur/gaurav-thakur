f5dac604bf74a99520062233db5284bf4654b56f
Gaurav Thakur <gthakur581@gmail.com>
2026-01-06 18:39:44 +0530

    Update: add `no-scrollbar` utility, improve links in About page, and update `nav` styles

--- src/app/_components/nav/nav.tsx
+++ src/app/_components/nav/nav.tsx
@@ -6,7 +6,7 @@ export function Nav() {
     <nav
       className={cn(
         "flex max-w-min rounded-full bg-white/90 px-1.5 py-1 text-sm font-medium text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm transition-transform duration-150 ease-out select-none",
-        "dark:bg-white/10 dark:text-white",
+        "no-scrollbar overflow-x-auto dark:bg-white/10 dark:text-white",
       )}
     >
       {navLinks.map((link) => {
